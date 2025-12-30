e1e70e53ce4d73e546dd087fb94acf8a94fb1bc5
sharo-jef <sharo.jef@gmail.com>
2025-12-30 20:30:38 +0900

    test: add test cases for language parameter priority

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -5,6 +5,15 @@ export default function DocsPage() {
     <div>
       Docs Page
       <CodeDisplay
+        language="mermaid"
+        filename="hoge.mermaid"
+        code={`graph TD;
+  A-->B;
+  A-->C;
+  B-->D;
+  C-->D;`}
+      />
+      <CodeDisplay
         language="python"
         filename="hoge.mermaid"
         code={`graph TD;
